0d7e0161efd3649f700dc56eadd7f0b31e0db2ce
David Riegel <40246197+davidriegel@users.noreply.github.com>
2026-05-13 10:24:53 +0200

    Initial Commit

---
+++ Tick/TickApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickApp.swift
+//  Tick
+//
+//  Created by David Riegel on 13.05.26.
+//
+
+import SwiftUI
+
+@main
+struct TickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
