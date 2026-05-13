ebdadc338f1b49c303950cd37fb3cb764716d132
David Riegel <40246197+davidriegel@users.noreply.github.com>
2026-05-13 17:24:33 +0200

    feat: implement token generator after rfc 4226

--- Tick/Services/TOTPGenerator.swift
+++ Tick/Services/TOTPGenerator.swift
@@ -9,5 +9,48 @@ import Foundation
 import CryptoKit
 
 enum TOTPGenerator {
+    static func generate(for token: OTPToken, at date: Date = Date()) -> String {
+        let counter = UInt64(date.timeIntervalSince1970) / UInt64(token.period)
+        return generateHOTP(secret: token.secret, counter: counter, algorithm: token.algorithm, digits: token.digits)
+    }
+    
+    fileprivate static func generateHOTP(secret: Data, counter: UInt64, algorithm: OTPAlgorithm, digits: Int) -> String {
+        var counterBE = counter.bigEndian
+        let counterData = Data(bytes: &counterBE, count: 8)
+        
+        let key = SymmetricKey(data: secret)
+        let hash: Data
+        
+        switch algorithm {
+        case .sha1:
+            hash = Data(HMAC<Insecure.SHA1>.authenticationCode(for: counterData, using: key))
+        case .sha256:
+            hash = Data(HMAC<SHA256>.authenticationCode(for: counterData, using: key))
+        case .sha512:
+            hash = Data(HMAC<SHA512>.authenticationCode(for: counterData, using: key))
+        }
+
+        // Dynamic truncation
+        let offset = Int(hash[hash.count - 1] & 0x0f)
+        let b0 = UInt32(hash[offset] & 0x7f)
+        let b1 = UInt32(hash[offset + 1])
+        let b2 = UInt32(hash[offset + 2])
+        let b3 = UInt32(hash[offset + 3])
+        
+        let code = (b0 << 24) | (b1 << 16) | (b2 << 8) | b3
+        
+        print(code)
+        
+        print(code % UInt32(pow(10.0, Double(digits))))
+        
+        let otp = code % UInt32(pow(10.0, Double(digits)))
+        var otpCode = String(otp)
+        
+        if otp < UInt32(pow(10.0, Double(digits) - 1)) {
+            otpCode = String(format: "%0\(digits)d", code)
+        }
+        
+        return otpCode
+    }
     
 }
